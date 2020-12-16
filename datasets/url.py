78c1a8ccada87e4c1255ca9715c61e8ef015f821
othmanefc <othmane.hassani1@gmail.com>
2020-12-16 16:15:04 +0100

    added req

--- datasets/url.py
+++ datasets/url.py
@@ -1,10 +1,13 @@
 URLS = {
     "triples.train.small":
-    "https://msmarco.blob.core.windows.net/msmarcoranking/triples.train.small.tar.gz",
+        "https://msmarco.blob.core.windows.net/msmarcoranking/triples.train"
+        ".small.tar.gz",
     "top1000.dev":
-    "https://msmarco.blob.core.windows.net/msmarcoranking/top1000.dev.tar.gz",
+        "https://msmarco.blob.core.windows.net/msmarcoranking/top1000.dev"
+        ".tar.gz",
     "top1000.eval":
-    "https://msmarco.blob.core.windows.net/msmarcoranking/top1000.eval.tar.gz",
+        "https://msmarco.blob.core.windows.net/msmarcoranking/top1000.eval"
+        ".tar.gz",
     "queries":
-    "https://msmarco.blob.core.windows.net/msmarcoranking/queries.tar.gz"
+        "https://msmarco.blob.core.windows.net/msmarcoranking/queries.tar.gz"
 }
